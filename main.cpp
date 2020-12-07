9694320e59d7ab95a8cab607c3d9148eb0fac2e9
Kuri_hg91@itstepru.onmicrosoft.com <Kuri_hg91@itstepru.onmicrosoft.com>
2020-12-07 19:27:05 +0500

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+void FillRand(int arr[], const int N);
+void Print(int arr[], const int N);
+
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	const int N = 5;
+	int arr[N]{};
+
+	FillRand(arr, N);
+	Print(arr, N);
+}
+
+void FillRand(int arr[], const int N)
+{
+	for (int i = 0; i < N; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+}
+
+void Print(int arr[], const int N)
+{
+	for (int i = 0; i < N; i++)
+	{
+		cout << arr[i] << "   ";
+	}
+	cout << endl;
+}
